a921e2969d6a293e3ed163b9c1040a04bdedf20e
samdy <samdyrupp@gmail.com>
2020-12-13 15:41:44 +0700

    initial time-tracker app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Tracker App',
+      home: Container(color: Colors.white),
+    );
+  }
+}
+
+void main() => runApp(MyApp());
